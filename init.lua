1c9b33ccc3e83a6708b220b7d0ff4ec02b5895d9
hank <ysh2291939848@outlook.com>
2025-04-02 17:39:17 +0800

    update

--- init.lua
+++ init.lua
@@ -2,17 +2,17 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 ---@diagnostic disable-next-line: undefined-field
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -86,13 +86,13 @@ vim.keymap.set("x", "k", "gk")
 vim.keymap.set("n", ";", ":")
 
 vim.keymap.set("n", "<M-n>", function()
-	require("snacks").terminal()
+  require("snacks").terminal()
 end, { desc = "Toggle terminal" })
 vim.keymap.set("t", "<M-n>", function()
-	require("snacks").terminal()
+  require("snacks").terminal()
 end, { desc = "Toggle terminal" })
 vim.keymap.set("n", "<leader>th", function()
-	require("snacks").terminal()
+  require("snacks").terminal()
 end, { desc = "Toggle terminal" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
@@ -113,289 +113,58 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower win
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 vim.keymap.set("n", "<leader>ui", function()
-	local input_avail, input = pcall(vim.fn.input, "Set indent value (>0 expandtab, <=0 noexpandtab): ")
-	if input_avail then
-		local indent = tonumber(input)
-		if not indent or indent == 0 then
-			return
-		end
-		vim.bo.expandtab = (indent > 0) -- local to buffer
-		indent = math.abs(indent)
-		vim.bo.tabstop = indent -- local to buffer
-		vim.bo.softtabstop = indent -- local to buffer
-		vim.bo.shiftwidth = indent -- local to buffer
-	end
+  local input_avail, input = pcall(vim.fn.input, "Set indent value (>0 expandtab, <=0 noexpandtab): ")
+  if input_avail then
+    local indent = tonumber(input)
+    if not indent or indent == 0 then
+      return
+    end
+    vim.bo.expandtab = (indent > 0) -- local to buffer
+    indent = math.abs(indent)
+    vim.bo.tabstop = indent         -- local to buffer
+    vim.bo.softtabstop = indent     -- local to buffer
+    vim.bo.shiftwidth = indent      -- local to buffer
+  end
 end)
 
 vim.cmd([[inoremap ( ()<Left>]])
 vim.cmd([[inoremap () ()]])
+
+vim.cmd([[inoremap ( ()<Left>]])
+vim.cmd([[inoremap () ()]])
+
+vim.cmd("inoremap { {}<Left>")
+vim.cmd("inoremap {} {}")
+
+vim.cmd("inoremap [ []<Left>")
+vim.cmd("inoremap [] []")
+
 vim.cmd([[set cmdheight=0]])
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
 -- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
 
 -- fix cursor in windows terminal
 vim.api.nvim_create_autocmd("VimLeave", {
-	pattern = "*",
-	callback = function()
-		vim.o.guicursor = ""
-		vim.fn.chansend(vim.v.stderr, "\x1b[ q]")
-	end,
+  pattern = "*",
+  callback = function()
+    vim.o.guicursor = ""
+    vim.fn.chansend(vim.v.stderr, "\x1b[ q]")
+  end,
 })
 
-local function has_words_before()
-	local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 require("lazy").setup({
-	spec = {
-		{
-			"rebelot/kanagawa.nvim",
-			config = function()
-				require("kanagawa").setup({
-					compile = true,
-					theme = "dragon",
-				})
-				vim.cmd("colorscheme kanagawa-dragon")
-			end,
-		},
-		{
-			"folke/snacks.nvim",
-			lazy = false,
-			---@type snacks.Config
-			opts = {
-				dashboard = {
-					enabled = true,
-					---@format disable-next
-					preset = {
-						header = [[
-⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷
-⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇
-⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽
-⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕
-⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕
-⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕
-⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄
-⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕
-⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿
-⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟
-⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠
-⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙
-⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣]],
-					},
-				},
-				image = {
-					enabled = true,
-				},
-				picker = {
-					enabled = true,
-					ui_select = true,
-					sources = {
-						explorer = {
-							layout = {
-								layout = {
-									width = 30,
-								},
-							},
-						},
-					},
-				},
-				input = {
-					enabled = true,
-				},
-				lazygit = {
-					enabled = true,
-				},
-				indent = {
-					enabled = true,
-				},
-				terminal = {
-					enabled = true,
-					win = {
-						height = 10,
-						position = "bottom",
-						style = "minimal",
-					},
-				},
-				---@class snacks.explorer.Config
-				explorer = {
-					enabled = true,
-					replace_netrw = true,
-				},
-			},
-			keys = {
-				{
-					"<leader>f<space>",
-					function()
-						require("snacks").picker.smart()
-					end,
-					desc = "Smart find files",
-				},
-				{
-					"<leader>ff",
-					function()
-						require("snacks").picker.files()
-					end,
-					desc = "Find files",
-				},
-				{
-					"<leader>fw",
-					function()
-						require("snacks").picker.grep()
-					end,
-					desc = "Grep files",
-				},
-				{
-					"<leader>g",
-					function()
-						require("snacks").lazygit()
-					end,
-					desc = "Lazygit",
-				},
-				{
-					"<leader>e",
-					function()
-						require("snacks").explorer()
-					end,
-					desc = "Explorer",
-				},
-			},
-		},
-		{
-			"nvim-treesitter/nvim-treesitter",
-			event = "BufReadPost",
-			opts = {
-				auto_install = true,
-				highlight = {
-					enable = true,
-				},
-			},
-		},
-		{
-			"saghen/blink.cmp",
-			event = "BufReadPost",
-			version = "*",
-			opts = {
-				keymap = {
-					["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
-					["<Up>"] = { "select_prev", "fallback" },
-					["<Down>"] = { "select_next", "fallback" },
-					["<C-N>"] = { "select_next", "show" },
-					["<C-P>"] = { "select_prev", "show" },
-					["<C-J>"] = { "select_next", "fallback" },
-					["<C-K>"] = { "select_prev", "fallback" },
-					["<C-U>"] = { "scroll_documentation_up", "fallback" },
-					["<C-D>"] = { "scroll_documentation_down", "fallback" },
-					["<C-e>"] = { "hide", "fallback" },
-					["<CR>"] = { "accept", "fallback" },
-					["<Tab>"] = {
-						"select_next",
-						"snippet_forward",
-						function(cmp)
-							if has_words_before() or vim.api.nvim_get_mode().mode == "c" then
-								return cmp.show()
-							end
-						end,
-						"fallback",
-					},
-					["<S-Tab>"] = {
-						"select_prev",
-						"snippet_backward",
-						function(cmp)
-							if vim.api.nvim_get_mode().mode == "c" then
-								return cmp.show()
-							end
-						end,
-						"fallback",
-					},
-				},
-				completion = {
-					list = { selection = { preselect = false, auto_insert = true } },
-					menu = {
-						auto_show = function(ctx)
-							return ctx.mode ~= "cmdline"
-						end,
-						border = "rounded",
-						winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-						draw = {
-							treesitter = { "lsp" },
-						},
-					},
-					accept = {
-						auto_brackets = { enabled = true },
-					},
-					documentation = {
-						auto_show = true,
-						auto_show_delay_ms = 0,
-						window = {
-							border = "rounded",
-							winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
-						},
-					},
-				},
-				signature = {
-					window = {
-						border = "rounded",
-						winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder",
-					},
-				},
-
-				appearance = {
-					use_nvim_cmp_as_default = true,
-					nerd_font_variant = "mono",
-				},
-				sources = {
-					default = { "lsp", "path", "snippets", "buffer" },
-				},
-
-				fuzzy = { implementation = "prefer_rust_with_warning" },
-			},
-		},
-		{
-			"nvimdev/guard.nvim",
-			dependencies = {
-				"nvimdev/guard-collection",
-			},
-			event = "BufReadPost",
-			opts = {},
-			config = function()
-				local ft = require("guard.filetype")
-
-				if vim.fn.executable("hlint") == 1 then
-					ft("haskell"):lint("hlint")
-				end
-
-				if vim.fn.executable("stylua") == 1 then
-					ft("lua"):fmt("stylua")
-				end
-
-				if vim.fn.executable("clang-tidy") == 1 then
-					ft("c"):lint("clang-tidy")
-					ft("cpp"):lint("clang-tidy")
-				end
-
-				vim.g.guard_config = {
-					fmt_on_save = "false",
-					lsp_as_default_formatter = true,
-				}
-			end,
-		},
-	},
-	ui = {
-		backdrop = 70,
-		border = "rounded",
-	},
+  spec = {
+    require('plugins')
+  },
+  ui = {
+    backdrop = 70,
+    border = "rounded",
+  },
 })
---
+++ lua/plugins.lua
@@ -0,0 +1,239 @@
+local function has_words_before()
+  local line, col = (unpack or table.unpack)(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    config = function()
+      require("kanagawa").setup({
+        compile = true,
+        theme = "dragon",
+      })
+      vim.cmd("colorscheme kanagawa-dragon")
+    end,
+  },
+  {
+    "folke/snacks.nvim",
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      dashboard = {
+        enabled = true,
+        ---@format disable-next
+					preset = {
+						header = [[
+⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷
+⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇
+⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽
+⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕
+⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕
+⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕
+⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄
+⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕
+⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿
+⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟
+⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠
+⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙
+⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣]],
+					},
+      },
+      image = {
+        enabled = true,
+      },
+      picker = {
+        enabled = true,
+        ui_select = true,
+        sources = {
+          explorer = {
+            layout = {
+              layout = {
+                width = 30,
+              },
+            },
+          },
+        },
+      },
+      input = {
+        enabled = true,
+      },
+      lazygit = {
+        enabled = true,
+      },
+      indent = {
+        enabled = true,
+      },
+      terminal = {
+        enabled = true,
+        win = {
+          height = 10,
+          position = "bottom",
+          style = "minimal",
+        },
+      },
+      ---@class snacks.explorer.Config
+      explorer = {
+        enabled = true,
+        replace_netrw = true,
+      },
+    },
+    keys = {
+      {
+        "<leader>f<space>",
+        function()
+          require("snacks").picker.smart()
+        end,
+        desc = "Smart find files",
+      },
+      {
+        "<leader>ff",
+        function()
+          require("snacks").picker.files()
+        end,
+        desc = "Find files",
+      },
+      {
+        "<leader>fw",
+        function()
+          require("snacks").picker.grep()
+        end,
+        desc = "Grep files",
+      },
+      {
+        "<leader>g",
+        function()
+          require("snacks").lazygit()
+        end,
+        desc = "Lazygit",
+      },
+      {
+        "<leader>e",
+        function()
+          require("snacks").explorer()
+        end,
+        desc = "Explorer",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = "BufReadPost",
+    opts = {
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    event = "BufReadPost",
+    version = "*",
+    opts = {
+      keymap = {
+        ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-N>"] = { "select_next", "show" },
+        ["<C-P>"] = { "select_prev", "show" },
+        ["<C-J>"] = { "select_next", "fallback" },
+        ["<C-K>"] = { "select_prev", "fallback" },
+        ["<C-U>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-D>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-e>"] = { "hide", "fallback" },
+        ["<CR>"] = { "accept", "fallback" },
+        ["<Tab>"] = {
+          "select_next",
+          "snippet_forward",
+          function(cmp)
+            if has_words_before() or vim.api.nvim_get_mode().mode == "c" then
+              return cmp.show()
+            end
+          end,
+          "fallback",
+        },
+        ["<S-Tab>"] = {
+          "select_prev",
+          "snippet_backward",
+          function(cmp)
+            if vim.api.nvim_get_mode().mode == "c" then
+              return cmp.show()
+            end
+          end,
+          "fallback",
+        },
+      },
+      completion = {
+        list = { selection = { preselect = false, auto_insert = true } },
+        menu = {
+          auto_show = function(ctx)
+            return ctx.mode ~= "cmdline"
+          end,
+          border = "rounded",
+          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+          draw = {
+            treesitter = { "lsp" },
+          },
+        },
+        accept = {
+          auto_brackets = { enabled = true },
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 0,
+          window = {
+            border = "rounded",
+            winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:None",
+          },
+        },
+      },
+      signature = {
+        window = {
+          border = "rounded",
+          winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder",
+        },
+      },
+
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "mono",
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+    },
+  },
+  {
+    "nvimdev/guard.nvim",
+    dependencies = {
+      "nvimdev/guard-collection",
+    },
+    event = "BufReadPost",
+    opts = {},
+    config = function()
+      local ft = require("guard.filetype")
+
+      if vim.fn.executable("hlint") == 1 then
+        ft("haskell"):lint("hlint")
+      end
+
+      if vim.fn.executable("stylua") == 1 then
+        ft("lua"):fmt("stylua")
+      end
+
+      if vim.fn.executable("clang-tidy") == 1 then
+        ft("c"):lint("clang-tidy")
+        ft("cpp"):lint("clang-tidy")
+      end
+
+      vim.g.guard_config = {
+        fmt_on_save = "false",
+        lsp_as_default_formatter = true,
+      }
+    end,
+  },
+}
